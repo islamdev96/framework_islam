1facaf2eeb7215ec6af717fb863ff77e7fa19492
islamdev96 <islamsayedglab@gmail.com>
2023-02-22 04:32:47 +0200

    la

---
+++ lib/NewApp/core/constant/localizetion/translate.dart
@@ -0,0 +1,31 @@
+import 'package:get/get.dart';
+
+class MyTranslation extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        "ar": {
+          "listOfServices": "قائمة الخدمات",
+          "googlePlay": "جوجل بلاي",
+          "socialMedia": "وسائل التواصل الاجتماعي",
+          "news": "أخبار",
+          "sport": "رياضة",
+          "programing": "برمجة",
+          "islamic": "إسلامي",
+          "freelancer": "العمل الحر",
+          "addFavorite": "اضف الى المفضلة",
+          "favorite": "المفضلة",
+        },
+        "en": {
+          "listOfServices": "List Of Services",
+          "googlePlay": "Google Play",
+          "socialMedia": "Social Media",
+          "news": "News",
+          "sport": "Sport",
+          "programing": "Programing",
+          "islamic": "Islamic",
+          "freelancer": "Freelancer",
+          "addFavorite": "Add Favorite",
+          "favorite": "Favorite",
+        }
+      };
+}
