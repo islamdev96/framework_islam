680e74bf9c44864c8ebb381d0bbbb5ddd402adf0
islamdev96 <islamsayedglab@gmail.com>
2023-04-09 03:43:56 +0200

    gr

---
+++ lib/new/navigation_widget/backgron_color_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// import '../../../core/constant/resources/color_manager.dart';
+
+// ignore: must_be_immutable
+class BackgroundColorPage extends StatelessWidget {
+  BackgroundColorPage({Key? key, this.text, this.color, this.onTap})
+      : super(key: key);
+  String? text;
+  Color? color;
+  Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    // color: ColorManager.kPrimary,
+                    blurRadius: 2.0,
+                    spreadRadius: 1.0,
+                    // offset: Offset(-1.0, 1.0),
+                    blurStyle: BlurStyle.solid),
+              ],
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.zero,
+                topRight: Radius.zero,
+                bottomLeft: Radius.circular(200.0),
+                // bottomRight: Radius.circular(200.0),
+              ),
+            ),
+            height: 75.0.h,
+            width: double.infinity,
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 30.h),
+              child: Text(
+                text!,
+                style: TextStyle(fontSize: 18.sp, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/new/navigation_widget/bottom_navigation_bar.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: unused_element, must_be_immutable
+
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// import '../../../core/constant/resources/color_manager.dart';
+// import '../../screen/favorite/favorite_home_page.dart';
+// import '../../screen/favorite/person.dart';
+// import '../../screen/home/home_page.dart';
+
+class BottomNavigationBarAll extends StatefulWidget {
+  const BottomNavigationBarAll({super.key});
+
+  @override
+  State<BottomNavigationBarAll> createState() => _BottomNavigationBarAllState();
+}
+
+class _BottomNavigationBarAllState extends State<BottomNavigationBarAll> {
+  int _currentIndex = 1;
+  final List<Widget> _children = [
+    // const Person(),
+    // const HomePage(),
+    // const FavoriteHomePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: CurvedNavigationBar(
+        // backgroundColor: ColorManager.backgroundcolor,
+        // color: ColorManager.kPrimary,
+        // buttonBackgroundColor: ColorManager.kPrimary,
+        index: _currentIndex,
+        key: const Key('bottomNavigationBar'),
+        letIndexChange: (index) => true,
+        animationDuration: const Duration(milliseconds: 1000),
+        animationCurve: Curves.easeInOutCubicEmphasized,
+        // index: true ? 3 : 1,
+        height: 48.h,
+        items: const <Widget>[
+          Icon(Icons.person, size: 25, color: Colors.white),
+          Icon(Icons.home, size: 25, color: Colors.white),
+          Icon(Icons.favorite, size: 25, color: Colors.white),
+        ],
+        onTap: onTabTapped,
+      ),
+    );
+  }
+
+////////
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
